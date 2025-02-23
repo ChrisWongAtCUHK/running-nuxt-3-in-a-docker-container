352a6c5f2ff1d89f2ebcb44a088dbb7ace3391c1
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-23 11:38:13 +0800

    NUXT_PUBLIC_GOOGLE_CLIENT_ID

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="center">
+    <GoogleSignInButton @success="onSuccess" @error="onError" />
   </div>
 </template>
+
+<script setup>
+// eslint-disable-next-line no-console
+const onSuccess = resp => console.log(resp)
+// eslint-disable-next-line no-console
+const onError = resp => console.error(resp)
+</script>
+
+<style>
+</style>
